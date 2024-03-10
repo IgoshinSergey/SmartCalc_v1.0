66a2a44787cc1a8089b4eb152f8e0c13fa3c2bb5
Sergey <sigoshin2@gmail.com>
2024-03-10 20:53:36 +0300

    Final

---
+++ src/build/CMakeFiles/SmartCalc.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/calculate.c" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/calculate.c.o" "gcc" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/calculate.c.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/parse.c" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/parse.c.o" "gcc" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/parse.c.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/polish_notation.c" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/polish_notation.c.o" "gcc" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/polish_notation.c.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/stack.c" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/stack.c.o" "gcc" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/stack.c.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/validation.c" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/validation.c.o" "gcc" "CMakeFiles/SmartCalc.dir/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/calculations/validation.c.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/build/SmartCalc_autogen/mocs_compilation.cpp" "CMakeFiles/SmartCalc.dir/SmartCalc_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/SmartCalc_autogen/mocs_compilation.cpp.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/qt/graph.cpp" "CMakeFiles/SmartCalc.dir/graph.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/graph.cpp.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/qt/main.cpp" "CMakeFiles/SmartCalc.dir/main.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/main.cpp.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/qt/mainwindow.cpp" "CMakeFiles/SmartCalc.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/mainwindow.cpp.o.d"
+  "/home/sergeyigoshin/School21/github/SmartCalc_v1.0/src/qt/qcustomplot.cpp" "CMakeFiles/SmartCalc.dir/qcustomplot.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/qcustomplot.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
